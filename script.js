1e9dfe0d59f7f1466c2177020245fe41d1024f75
priya-1007 <2405824@kiit.ac.in>
2026-05-03 12:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,23 @@ async function getWeather() {
     }
 
     let apiKey = "8d0661da0eb3faaab5343be82aa742b6";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
         let response = await fetch(url);
         let data = await response.json();
 
-    
         if (data.cod !== 200) {
             document.getElementById("result").innerHTML = data.message;
             return;
         }
 
         document.getElementById("result").innerHTML = `
-    <h2>${data.name}</h2>
-    <p>Temperature: ${data.main.temp}°C</p>
-    <p>Weather: ${data.weather[0].description}</p>
-    <p>Humidity: ${data.main.humidity}%</p>
-`;
+            <h2>${data.name}</h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+        `;
     } catch (error) {
         document.getElementById("result").innerHTML = "Error fetching data";
     }
